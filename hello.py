dd0b1580cc8fc831958a667341f23f369e2f6e5b
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-04-23 14:56:21 +0300

    Variables finished

--- hello.py
+++ hello.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python3
+print('Hello World\n')
+
+print("Double-quoted Hello World")
+
+print('conca'+'tenation')
+
+print('conca','tenation')
+
+print('I am', 5)
+
+print('I\'m 5')
+
+print("He said \"Hi\"")
 
 
+print(1+3)
 
-print('Hello World')
+print(1-3)
+
+print(1*3)
+
+print(1/3)
+
+print(4.2*6.6)
+
+print(9**4)
+
+print(5%2)
+
+var = 100
+
+print(var)
+
+strvar = '3000'
+
+print(strvar)
+
+print(strvar[0])
