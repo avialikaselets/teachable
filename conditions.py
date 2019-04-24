7457d312bd4ce2965a5915acbb85d9cb3357b7c8
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-04-24 18:57:00 +0300

    Included conditions.py after "if" statement

---
+++ conditions.py
@@ -0,0 +1,39 @@
+i = 1
+
+while i < 10:
+    print(i)
+    #i = i+1
+    i += 1
+    '''
+    multiple
+    lines
+    go
+    here
+    '''
+
+print("\n\n\n'for' statement starting\n\n\n")
+
+exampleList = [1,7,8,4,9,0,2,3]
+
+for i in exampleList:
+    print(i)
+
+
+print("\n\n\nNew example of 'for' statement\n\n\n")
+
+for i in range(1,11):
+    print(i)
+
+
+x = 2
+y = 7
+z = 10
+
+if x > y:
+    print(x,'is greater then',y)
+
+if x < y:
+    print(x,'is less then',y)    
+
+if x == '2':
+    print(x,"is equal to",y)
