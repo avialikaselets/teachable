33e05332509121401a316f0d8e4c5d332bf8c467
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-04-30 08:39:28 +0000

    File handling is rewritten to use "with"

--- files.py
+++ files.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-
+'''
 data = 'changed text'
 
 file = open('output.txt', 'w')
 file.write(data)
 file.close()
+'''
+
+dataset = [1,4,5,88,44,'ololo','ololox2',3940]
+#file = open('output.txt', 'a')
+
+with open('output.txt', 'a') as file:
+    for i in dataset:
+        file.write(str(i))
+        file.write('\n')
+
+print(file.closed)
