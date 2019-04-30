7cbf901b01d57ba19ed29a699810ef6562ad4176
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-04-30 09:38:15 +0000

    Added "read file" operation

--- files.py
+++ files.py
@@ -9,10 +9,19 @@ file.close()
 
 dataset = [1,4,5,88,44,'ololo','ololox2',3940]
 #file = open('output.txt', 'a')
-
+'''
 with open('output.txt', 'a') as file:
     for i in dataset:
         file.write(str(i))
         file.write('\n')
 
 print(file.closed)
+'''
+
+with open('output.txt', 'r') as file:
+    text_data = file.readlines()
+
+for s in text_data:
+    text_data[text_data.index(s)] = s.replace('\n', '')
+
+print(text_data)
