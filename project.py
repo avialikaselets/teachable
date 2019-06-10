a6ab179b9b6059bb602aa56175675ceddc635803
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-06-10 18:20:48 +0300

    Moved "while True:" out of main() function.

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-from statistics import *
+from statistics import mean
+
+ADMIN_CREDENTIALS = {"admin": "pass",
+                     "administrator": "password"}
 
 students = {'Jeremy': [8, 8],
             'Ann': [4, 8],
@@ -18,25 +21,40 @@ def EnterGrade(student_name, grade):
 def main():
     name = ""
     grade = 0
+    print("Select operation:",
+          "1. Enter grade",
+          "2. Add student",
+          "3. Delete student",
+          "4. Print the list of students",
+          "5. Calculate average grade for a student",
+          "6. Exit", sep = "\n")
+    ctrl = input("Type your choice: ")
+    print("\n")
+    if ctrl == "6":
+        exit()
+    if ctrl == "1":
+        name = input("Student's name: ")
+        grade = input("Enter grade: ")
+        EnterGrade(name, int(grade))
+    if ctrl == "2":
+        name = input("Type student's name: ")
+        AddStudent(name)
+    if ctrl == "3":
+        name = input("Type student's name: ")
+        DeleteStudent(name)
+    if ctrl =="4":
+        print(students)
+    if ctrl =="5":
+        name = input("Type student's name: ")
+        print(mean(students[name]))
+    print("\n")
+
+username = ""
+password = ""
+username = input("Input username: ")
+password = input("Input password: ")
+if username in ADMIN_CREDENTIALS and password == ADMIN_CREDENTIALS[username]:
     while True:
-        print("Select operation:\n1. Enter grade\n2. Add student\n3. Delete student\n4. Print the list of students\n5. Calculate average grade for a student\n6. Exit")
-        ctrl = input("Type your choice:")
-        print("\n")
-        if ctrl == "6":
-            break
-        if ctrl == "1":
-            name = input("Student's name:")
-            grade = input("Enter grade:")
-            EnterGrade(name, int(grade))
-        if ctrl == "2":
-            name = input("Type student's name:")
-            AddStudent(name)
-        if ctrl == "3":
-            name = input("Type student's name:")
-            DeleteStudent(name)
-        if ctrl =="4":
-            print(students)
-        if ctrl =="5":
-            name = input("Type student's name:")
-            print(mean(students[name]))
-main()
+        main()
+else:
+    print("Wrong credentials!")
