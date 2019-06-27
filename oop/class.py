017abbd75459019e0741f86db0279dd6b5016917
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-06-27 19:32:39 +0300

    Changed all class properties to private.

--- oop/class.py
+++ oop/class.py
@@ -8,34 +8,34 @@ class BankAccount:
                  x_last_name,
                  x_account_number,
                  x_deposit_amount):
-        self.first_name = x_first_name
-        self.last_name = x_last_name
-        self.account_number = x_account_number
-        self.deposit_amount = x_deposit_amount
+        self.__first_name = x_first_name
+        self.__last_name = x_last_name
+        self.__account_number = x_account_number
+        self.__deposit_amount = x_deposit_amount
 
     def get_first_name(self):
-        return self.first_name
+        return self.__first_name
 
     def get_last_name(self):
-        return self.last_name
+        return self.__last_name
 
     def get_account_number(self):
-        return self.account_number
+        return self.__account_number
 
     def get_deposit_amount(self):
-        return self.deposit_amount
+        return self.__deposit_amount
 
     def set_first_name(self, x_first_name):
-        self.first_name = x_first_name
+        self.__first_name = x_first_name
 
     def set_last_name(self, x_last_name):
-        self.last_name = x_last_name
+        self.__last_name = x_last_name
 
     def set_account_number(self, x_account_number):
-        self.account_number = x_account_number
+        self.__account_number = x_account_number
 
     def set_deposit_amount(self, x_deposit_amount):
-        self.deposit_amount = x_deposit_amount
+        self.__deposit_amount = x_deposit_amount
 
 
 def main():
@@ -52,10 +52,18 @@ def main():
         print("\n")
 
     for client in clients:
-        print("First name: ", client.first_name, "\n"
-              "Last name: ", client.last_name, "\n"
-              "Account number: ", client.account_number, "\n"
-              "Deposit amount: ", client.deposit_amount)
+        print("First name: ", client.get_first_name(), "\n"
+              "Last name: ", client.get_last_name(), "\n"
+              "Account number: ", client.get_account_number(), "\n"
+              "Deposit amount: ", client.get_deposit_amount())
+
+    clients[1].set_first_name("Jerry")
+
+    for client in clients:
+        print("First name: ", client.get_first_name(), "\n"
+              "Last name: ", client.get_last_name(), "\n"
+              "Account number: ", client.get_account_number(), "\n"
+              "Deposit amount: ", client.get_deposit_amount())
 
 
 main()
