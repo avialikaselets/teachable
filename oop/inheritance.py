19c3655ba57ebc8bd25e076df0209d06a0fabf48
Andrei Vialikaselets <47030306+avialikaselets@users.noreply.github.com>
2019-07-01 15:55:30 +0300

    Initial commit to inheritance.py

---
+++ oop/inheritance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+class BankAccount:
+
+    def __init__(self, x_deposit_amount):
+        self.__deposit_amount = x_deposit_amount
+
+    def get_deposit_amount(self):
+        return self.__deposit_amount
+
+    def deposit_funds(self, x_amount):
+        self.__deposit_amount += x_amount
+
+    def withdraw_funds(self, x_amount):
+        self.__deposit_amount -= x_amount
+
+
+class SavingsAccount(BankAccount):
+
+    def __init__(self, x_deposit_amount):
+        BankAccount.__init__(self, x_deposit_amount)
+
+
+def main():
+    account_01 = BankAccount(500)
+    print(account_01.get_deposit_amount())
+
+    savings_account_01 = SavingsAccount(300)
+    print(savings_account_01.get_deposit_amount())
+
+
+main()
